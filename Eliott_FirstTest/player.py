add27a47ad29718b7a75e9c5e17057614e7ad791
MasterDiamant <masterdiamant2008@gmail.com>
2025-04-18 14:33:39 +0200

    Functionalised player movement

--- Eliott_FirstTest/player.py
+++ Eliott_FirstTest/player.py
@@ -1,4 +1,5 @@
 from world import*
+from math import*
 import pyxel
 class Player:
 
@@ -16,47 +17,29 @@ class Player:
         
     def update(self):
         if pyxel.btn(pyxel.KEY_Q):
-            self.left()
+            self.move([-1,0])
         if pyxel.btn(pyxel.KEY_D):
-            self.right()
+            self.move([1,0])
         if pyxel.btn(pyxel.KEY_Z):
-            self.up()
+            self.move([0,-1])
         if pyxel.btn(pyxel.KEY_S):
-            self.down()
+            self.move([0,1])
         self.tile_y = int(self.y/TILE_SIZE)
         self.tile_x = int(self.x/TILE_SIZE)
 
-    def left(self):
-        new_x = self.x - self.VELOCITY
-        new_tile_x = self.tile_x-1
-        next_tile_up = self.world.world_map[self.tile_y][new_tile_x]
-        next_tile_down = self.world.world_map[self.tile_y+1][new_tile_x]
-        if (next_tile_up != WorldItem.WALL or not collision(new_x, self.y, new_tile_x*TILE_SIZE, self.tile_y*TILE_SIZE)) and (next_tile_down != WorldItem.WALL or not collision(new_x, self.y, new_tile_x*TILE_SIZE, (self.tile_y+1)*TILE_SIZE)):
-                self.x = new_x
+    def move(self, direction):
+        new_x = self.x + self.VELOCITY*direction[0]
+        new_y = self.y + self.VELOCITY*direction[1]
+        
+        new_tile_x = self.tile_x+direction[0]
+        new_tile_y = self.tile_y+direction[1]
 
-    def right(self):
-        new_x = self.x + self.VELOCITY
-        new_tile_x = self.tile_x+1
-        next_tile_up = self.world.world_map[self.tile_y][new_tile_x]
-        next_tile_down = self.world.world_map[self.tile_y+1][new_tile_x]
-        if (next_tile_up != WorldItem.WALL or not collision(new_x, self.y, new_tile_x*TILE_SIZE, self.tile_y*TILE_SIZE)) and (next_tile_down != WorldItem.WALL or not collision(new_x, self.y, new_tile_x*TILE_SIZE, (self.tile_y+1)*TILE_SIZE)):
-                self.x = new_x
-    
-    def up(self):
-        new_y = self.y - self.VELOCITY
-        new_tile_y = self.tile_y-1
-        next_tile_left = self.world.world_map[new_tile_y][self.tile_x]
-        next_tile_right = self.world.world_map[new_tile_y][self.tile_x+1]
-        if (next_tile_left != WorldItem.WALL or not collision(self.x, new_y, self.tile_x*TILE_SIZE, new_tile_y*TILE_SIZE)) and (next_tile_right != WorldItem.WALL or not collision(self.x, new_y, (self.tile_x+1)*TILE_SIZE, new_tile_y*TILE_SIZE)):
-                self.y = new_y
+        next_tile_1 = self.world.world_map[new_tile_y][new_tile_x]
+        next_tile_2 = self.world.world_map[new_tile_y+abs(direction[0])][new_tile_x+abs(direction[1])]
 
-    def down(self):
-        new_y = self.y + self.VELOCITY
-        new_tile_y = self.tile_y+1
-        next_tile_left = self.world.world_map[new_tile_y][self.tile_x]
-        next_tile_right = self.world.world_map[new_tile_y][self.tile_x+1]
-        if (next_tile_left != WorldItem.WALL or not collision(self.x, new_y, self.tile_x*TILE_SIZE, new_tile_y*TILE_SIZE)) and (next_tile_right != WorldItem.WALL or not collision(self.x, new_y, (self.tile_x+1)*TILE_SIZE, new_tile_y*TILE_SIZE)):
-                self.y = new_y
+        if (next_tile_1 != WorldItem.WALL or not collision(new_x, new_y, new_tile_x*TILE_SIZE, new_tile_y*TILE_SIZE)) and (next_tile_2 != WorldItem.WALL or not collision(new_x, new_y, (new_tile_x+abs(direction[1]))*TILE_SIZE, (new_tile_y+abs(direction[0]))*TILE_SIZE)):
+            self.x = new_x
+            self.y = new_y
 
 def collision(x1,y1,x2,y2):
         return x1+TILE_SIZE>x2 and x2+TILE_SIZE>x1 and y1+TILE_SIZE>y2 and y2+TILE_SIZE>y1
