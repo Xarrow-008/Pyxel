cd02a05948186e47063605c78f7e6b21d4419af1
Xarrow-008 <leox.pages@gmail.com>
2025-05-02 23:29:47 +0200

    need fix but me ned slep

--- notAScrap/main.py
+++ notAScrap/main.py
@@ -13,7 +13,6 @@ class App:
         os.system('cls')
         pyxel.init(CAMERA_WIDTH, CAMERA_HEIGHT, title="Not a scrap")
         pyxel.load("../notAScrap.pyxres")
-        
         self.world = World(pyxel.tilemaps[0])
 
         pyxel.mouse(True)
@@ -59,28 +58,35 @@ class World:
         last_down = False
         X_tile = CAMERA_WIDTH//16 - 2
         Y_tile = 0
-        square_append(self.world_map, X_tile, 0)
-        for i in range(25):
+        W_rect = 3
+        H_rect = 3
+        rect_append(self.world_map, X_tile, 0, 3, 3)
+        for i in range(25):              #need fis prolly wipe need sleep now
             if last_down:
                 sq_pos = random.randint(0,2)
                 if sq_pos == 0 and X_tile>0:
+                    self.world_map[Y_tile+H_rect//2+1][X_tile-H_rect//2+1] = (9,2)
                     X_tile -= 4
                     last_down = False
                 elif sq_pos == 1 and X_tile<CAMERA_WIDTH:
+                    self.world_map[Y_tile+H_rect//2+1][X_tile+W_rect] = (9,2)
                     X_tile += 4
                     last_down = False
                 else:
-                    Y_tile += 4    
+                    self.world_map[Y_tile+H_rect][X_tile+W_rect//2+1] = (9,3) 
+                    Y_tile += 4 
             else:
-                Y_tile += 4
+                self.world_map[Y_tile+H_rect][X_tile+W_rect//2+1] = (9,3)
+                Y_tile += 4 
                 last_down = True
-            square_append(self.world_map, X_tile ,Y_tile)
+
+            W_rect = random.randint(3,9)
+            H_rect = random.randint(3,9)
+            rect_append(self.world_map, X_tile ,Y_tile,W_rect,H_rect)
             
-
-
-def square_append(tab, x, y):
-    for in_y in range(3):
-        for in_x in range(3):
+def rect_append(tab, x, y, w, h):
+    for in_y in range(h):
+        for in_x in range(w):
             tab[y+in_y][x+in_x] = WorldItem.GROUND
 
 
