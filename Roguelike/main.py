04f8dfd44fd13d95b9b3e5affe1cbfd9476932b1
MasterDiamant <masterdiamant2008@gmail.com>
2025-05-02 19:48:14 +0200

    Added player class to Roguelike

--- Roguelike/main.py
+++ Roguelike/main.py
@@ -14,6 +14,7 @@ class App:
         pyxel.load("../roguelike.pyxres")
         
         self.world = World(pyxel.tilemaps[0])
+        self.player = Player(self.world)
 
         pyxel.mouse(True)
 
@@ -21,10 +22,13 @@ class App:
 
     def update(self):
 
+        self.player.update()
+
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
         if pyxel.btnp(pyxel.KEY_R):
-            print("reset")
+            self.world = World(pyxel.tilemaps[0])
+            self.player = Player(self.world)
 
     def draw(self):
         pyxel.cls(0)
@@ -69,5 +73,37 @@ def draw_tile(pyxel, x, y, tile):
                 TILE_SIZE,
                 TILE_SIZE
                 )
+    
+class Player:
+    def __init__(self, world):
+        self.x = 0
+        self.y = 0
+        self.world = world
+        self.physics = Physics
+
+    def update(self):
+        print("update")
+
+class Physics:
+    def __init__(self):
+        self.speed = 0
+
+    def move(self, movements):
+        new_x = self.x + self.speed*movements[0]
+        new_y = self.y + self.speed*movements[1]
+        
+        new_tile_x = self.tile_x + pyxel.sgn(movements[0])
+        new_tile_y = self.tile_y + pyxel.sgn(movements[1])
+
+        next_tile_1 = self.world.world_map[new_tile_y][new_tile_x]
+        next_tile_2 = self.world.world_map[new_tile_y+abs(pyxel.sgn(movements[0]))][new_tile_x+abs(pyxel.sgn(movements[1]))]
+
+        if (next_tile_1 != WorldItem.WALL or not collision(new_x, new_y, new_tile_x*TILE_SIZE, new_tile_y*TILE_SIZE)) and (next_tile_2 != WorldItem.WALL or not collision(new_x, new_y, (new_tile_x+abs(direction[1]))*TILE_SIZE, (new_tile_y+abs(direction[0]))*TILE_SIZE)):
+            self.x = new_x
+            self.y = new_y
+    
+    def collision(x1,y1,x2,y2):
+        return x1+TILE_SIZE>x2 and x2+TILE_SIZE>x1 and y1+TILE_SIZE>y2 and y2+TILE_SIZE>y1
+
 
 App()
