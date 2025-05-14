8641af5a792e45d78acd2dc5b7266626c0ec75c2
Xarrow-008 <leox.pages@gmail.com>
2025-05-14 16:59:24 +0200

    basics

---
+++ new_training/mini_games.py
@@ -0,0 +1,16 @@
+import pyxel, os, math, random
+
+class Game:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+
+class World:
+    def __init__(self,tilemap,game_state):
+        self.tilemap = tilemap
+
+class Camera:
+    def __init__
