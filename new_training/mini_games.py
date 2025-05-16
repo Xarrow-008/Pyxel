0d33691c1cacecaff4768385e5c369b61bd38314
Xarrow-008 <leox.pages@gmail.com>
2025-05-16 10:35:31 +0200

    push tile size

--- new_training/mini_games.py
+++ new_training/mini_games.py
@@ -1,19 +1,24 @@
 import pyxel, math, random, os
 
+TILE_SIZE = 8
+
 WIDTH = 64
 HEIGHT = 64
 
-CAM_W = 8*8
-CAM_H = 8*8
+CAM_W = 12*8
+CAM_H = 12*8
 
 class App:
     def __init__(self):
         pyxel.init(CAM_W,CAM_H,title='Hauted Shooter',fps=120)
         pyxel.load('../training.pyxres')
+
+
+        pyxel.run(self.update,self.draw)
     def update(self):
         pass
     def draw(self):
-        pyxel.bltm(0,0,0,)
+        pyxel.bltm(0,0,0,0,0,WIDTH*TILE_SIZE,HEIGHT*TILE_SIZE)
 
 class World:
     def __init__(self):
